b2ffb3f9575b695746013d9cca882dc2f7df5eb8
David Konsumer <konsumer@jetboystudio.com>
2019-02-07 14:31:19 -0800

    removing prepareCache, in hopes that non-js files get put in correct place

--- example/graphql.js
+++ example/graphql.js
@@ -1,7 +1,7 @@
 const { fileLoader, mergeTypes, mergeResolvers } = require('merge-graphql-schemas')
 
-const resolvers = mergeResolvers(fileLoader(`./resolvers/**/*.js`))
-const typeDefs = mergeTypes(fileLoader(`./schema/**/*.graphql`), { all: true })
+const resolvers = mergeResolvers(fileLoader(`${__dirname}/resolvers/**/*.js`))
+const typeDefs = mergeTypes(fileLoader(`${__dirname}/schema/**/*.graphql`), { all: true })
 
 module.exports = {
   typeDefs,
