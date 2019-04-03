634c1feaa690d96443b2419002c4e497a200eb8a
David Konsumer <konsumer@jetboystudio.com>
2019-04-02 19:10:07 -0700

    still trying to fix subdir apps

--- server/index.js
+++ server/index.js
@@ -1,11 +1,17 @@
 const express = require('express')
 const { ApolloServer } = require('apollo-server-express')
 
-let config = require('./_entrypoint')
-
-const server = new ApolloServer(config)
+// this is the entrypoint code
+const config = require('./_entrypoint')
 
 const app = express()
-server.applyMiddleware({ app })
+
+// load a sub-app for greater control
+const childApp = express()
+const path = config.path || '/graphql'
+const server = new ApolloServer(config)
+server.applyMiddleware({ childApp, path })
+
+app.use(path, childApp)
 
 module.exports = app
